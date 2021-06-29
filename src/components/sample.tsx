2da67a94916d2bc23e1cb2520b9a97d59cec5161
satya-vinay <satyavinay2@gmail.com>
2021-06-30 00:01:39 +0530

    chakra ui trial

--- src/components/sample.tsx
+++ src/components/sample.tsx
@@ -1,5 +1,21 @@
+import { FormControl, FormHelperText, FormLabel, HStack, Input, Radio, RadioGroup } from "@chakra-ui/react"
+import { Component } from "react"
+
 function Sample(){
-    return <div>Sample</div>
+    return (
+        <FormControl as="fieldset" className=".">
+  <FormLabel as="legend">Favorite  Card suit</FormLabel>
+  <RadioGroup defaultValue="Itachi">
+    <HStack spacing="24px">
+      <Radio value="Clubs">Clubs</Radio>
+      <Radio value="Diamonds">Diamonds</Radio>
+      <Radio value="Hearts">Hearts</Radio>
+      <Radio value="Spades">Spades</Radio>
+    </HStack>
+  </RadioGroup>
+  <FormHelperText>Select only if you play blackjack.</FormHelperText>
+</FormControl>
+    )
 }
 
 export default Sample
